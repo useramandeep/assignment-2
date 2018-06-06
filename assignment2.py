8f9363b0d11a499638576179f291ecb6c3e435b6
useramandeep <upworkaman@gmail.com>
2018-06-06 23:55:04 +0530

    First Assignment

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+print("Hello This Is My Output") #Q.1- Print anything you want on screen
+                                   #output: Hello This Is My Output
+
+print("Acad"+"View")               Q.2- Join two strings using '+'.
+                                   #E.g.-"Acad"+"View”
+                                   #Output: AcadView
+
+a=5                                #Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+b=6
+c=7
+print(a,b,c)                       #output: 5 6 7
+
+print('Let\'s get started')  #Q.4- Print “Let’s get started” on screen.
+                              #Output: Let's get started
+
+s="Acadview"
+course="Python"
+fees=5000
+print("Hello welcome to %s your course name is %s and total amount pending Rs %d"%(s,course,fees))  #Q.5- Print the following values using placeholders.
+                                                                                                    #Output: Hello welcome to Acadview your course name is Python and total amount pending 5000
+
+
+name="Tony Stark"             #Q.6- Let’s do some interesting exercise:
+salary=1000000
+print('%s''%d'%(name,salary)) #output: Tony Stark1000000
